772f3a23958dfb862aab7ec1d87119142c688117
Jonathan Birge <birge@mit.edu>
2022-01-14 15:27:48 -0500

    clean up autoconf files

--- makeclean.sh
+++ makeclean.sh
@@ -2,11 +2,7 @@ rm -f aclocal.m4
 rm -rf autom4te.cache
 rm -rf .deps
 rm -f configure
-rm -f depcomp
-rm -f install-sh
-rm -f missing
-rm -f Makefile.in
+rm -f depcomp install-sh missing
+rm -f Makefile.in Makefile
 rm -f compile
-rm -f config.log
-rm -f config.status
-
+rm -f config.log config.status
