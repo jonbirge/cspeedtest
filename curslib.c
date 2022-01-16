a28634f8c05208673ebe38bf2590427901f288e0
Jonathan Birge <birge@mit.edu>
2022-01-15 22:05:16 -0500

    rotating dots for non-random display

--- curslib.c
+++ curslib.c
@@ -3,7 +3,31 @@
 #include <sys/time.h>
 #include <math.h>
 
-void drawbar(double frac, int width, int line, int offset)
+#define NP 1024
+
+
+/*** Library internal functions ***/
+
+// Return random integer between 0 and max, inclusive. Placeholder for more uniform implementation.
+int rand_max (int max)
+{
+   return rand() % (max + 1);
+}
+
+// Pass through integer with limits.
+int limiter (int in, int min, int max)
+{
+   if (in > max)
+      return max;
+   if (in < min)
+      return min;
+   return in;
+}
+
+
+/*** Library external functions ***/
+
+void drawbar (double frac, int width, int line, int offset)
 {
    int j;
    
@@ -20,7 +44,7 @@ void drawbar(double frac, int width, int line, int offset)
    addch (']');
 }
 
-void drawline(int row, int width)
+void drawline (int row, int width)
 {
    move (row, 0);
    clrtoeol ();
@@ -29,7 +53,7 @@ void drawline(int row, int width)
 }
 
 // Completely random matrix of data
-void write_matrix(int nrows, int ncols, int docolor)
+void write_matrix (int nrows, int ncols, int docolor)
 {
    int r, c, attrb;
 
@@ -51,58 +75,56 @@ void write_matrix(int nrows, int ncols, int docolor)
 }
 
 // Compressible matrix of data
-void write_matrix_comp(int nrows, int ncols, int docolor)
+void write_matrix_comp (int nrows, int ncols, int docolor)
 {
    double slow;  // factor to slow animation
-   register int r, c, attrb, q = 0;
-   static int phase;
+   register int row, col, attrb;
+   static int init;
+   static double rs[NP], phis[NP];
 
-   if (docolor)
-      slow = 8;
-   else
-      slow = 256;
-
-   if (phase/slow < ncols)
-      phase++;
-   else
-      phase = 1;
-   attron (A_BOLD);
-   for (r = 2; r < nrows - 2; ++r)
-   {
-      move (r, 0);
-      for (c = 0; c < ncols; ++c)
+   if (init)
+   {  // rotate
+      for (int k = 0; k < NP; k++)
       {
-         if ((++q % (int) ceil(phase/slow)) == 0)
-         {
-            if (docolor)
-            {
-               attroff (COLOR_PAIR(3));
-               attron (COLOR_PAIR(6));
-               addch ('#');
-               attroff (COLOR_PAIR(6));
-               attron (COLOR_PAIR(3));
-            }
-            else
-            {
-               addch ('#');
-            }
-         }
-         else
-         {
-            if (docolor)
-            {
-               attrb = COLOR_PAIR((rand () & 0x0002) + 1);
-               attron (attrb);
-            }
-            addch('.');
-         }
+         phis[k] += 0.0001;
       }
+   }
+   else
+   {  // init
+      for (int k = 0; k < NP; k++)
+      {
+         rs[k] = (double) rand_max ( ceil(ncols/2) );
+         phis[k] = rand_max(359) / 3.1456 * 180.0;
+      }
+      init = 1;
+   }
+
+   // clear
+   for (row = 2; row < nrows - 2; row++)
+   {
+      move (row, 0);
+      clrtoeol();
+   }
+
+   // write field
+   attron (A_BOLD);
+   for (int k = 0; k < NP; k++)
+   {
+      col = round(rs[k]*cos(phis[k]) + ncols/2.0);
+      row = round(rs[k]*sin(phis[k])/2.0 + nrows/2.0);
+      move (limiter(row, 2, nrows - 3), limiter(col, 1, ncols));
+      if (docolor)
+      {
+         attrb = COLOR_PAIR((k % 8) + 1);
+         attron(attrb);
+      }
+      addch ('.');
    }
    attroff (A_BOLD);
 }
 
 // Track and display bitrate
-void display_mbps(int dk, int nrows, int ncols, int docolor, int reset)
+void display_mbps (int dk, int nrows, int ncols, int docolor, int reset)
 {
    static int sec, us, secold = 0, usold = 0;
    struct timeval systime;
@@ -147,7 +169,7 @@ void display_mbps(int dk, int nrows, int ncols, int docolor, int reset)
    attroff(COLOR_PAIR(1));
 }
 
-void static_display(int nrows, int ncols, int docolor, int docomp)
+void static_display (int nrows, int ncols, int docolor, int docomp)
 {
    attron(COLOR_PAIR(1));
    drawline (1, ncols);
@@ -168,7 +190,7 @@ void static_display(int nrows, int ncols, int docolor, int docomp)
    }
    else
    {
-      attron (COLOR_PAIR(2));
+      attron (COLOR_PAIR(16));
       printw("random");
       attron (COLOR_PAIR(1));
    }
@@ -179,7 +201,7 @@ void static_display(int nrows, int ncols, int docolor, int docomp)
    printw(" to toggle ");
    if (docolor)
    {
-      attron (COLOR_PAIR(2));
+      attron (COLOR_PAIR(16));
       printw("color");
       attron (COLOR_PAIR(1));
    }
@@ -191,14 +213,22 @@ void static_display(int nrows, int ncols, int docolor, int docomp)
    attroff(COLOR_PAIR(1));
 }
 
-void init_colors()
+void init_colors ()
 {
    init_pair (1, COLOR_WHITE, COLOR_BLACK);
-   init_pair (2, COLOR_BLACK, COLOR_WHITE);
+   init_pair (2, COLOR_WHITE, COLOR_BLACK);
    init_pair (3, COLOR_CYAN, COLOR_BLACK);
    init_pair (4, COLOR_MAGENTA, COLOR_BLACK);
    init_pair (5, COLOR_YELLOW, COLOR_BLACK);
-   init_pair (6, COLOR_BLACK, COLOR_CYAN);
-   init_pair (7, COLOR_BLACK, COLOR_MAGENTA);
-   init_pair (8, COLOR_BLACK, COLOR_YELLOW);
+   init_pair (6, COLOR_RED, COLOR_BLACK);
+   init_pair (7, COLOR_GREEN, COLOR_BLACK);
+   init_pair (8, COLOR_BLUE, COLOR_BLACK);
+   init_pair (9, COLOR_BLACK, COLOR_CYAN);
+   init_pair (10, COLOR_BLACK, COLOR_MAGENTA);
+   init_pair (11, COLOR_BLACK, COLOR_YELLOW);
+   init_pair (12, COLOR_BLACK, COLOR_RED);
+   init_pair (13, COLOR_BLACK, COLOR_GREEN);
+   init_pair (14, COLOR_BLACK, COLOR_BLUE);
+   init_pair (15, COLOR_CYAN, COLOR_BLUE);
+   init_pair (16, COLOR_BLACK, COLOR_WHITE);
 }
