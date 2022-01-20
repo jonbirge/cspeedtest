5aee56e08a9a74be09e5141e25cbcd0ecf93c4f2
Jonathan Birge <birge@mit.edu>
2022-01-20 10:16:57 -0500

    improved autoconf script

--- makeclean.sh
+++ makeclean.sh
@@ -1,9 +1,7 @@
+rm -f *.o
 rm -f aclocal.m4
 rm -rf autom4te.cache
 rm -rf .deps
-rm -f configure
 rm -f depcomp install-sh missing
 rm -f Makefile.in Makefile
-rm -f compile
-rm -f config.log config.status
-rm -f config.h config.h.in stamp-* null.d
+rm -f compile config.log config.status config.h config.h.in stamp-* null.d configure
