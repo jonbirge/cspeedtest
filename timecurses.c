8a23eb8906d9054d43292e8b34a85726fdfc9ba6
Jonathan Birge <birge@mit.edu>
2022-02-16 17:48:47 -0800

    split screens into individual files

--- timecurses.c
+++ timecurses.c
@@ -5,17 +5,24 @@
 #include "curslib.h"
 #include "timecurses.h"
 
+#include "random_screen.h"
+#include "swirl_screen.h"
+
 
 // Structure of all displays
 const int screen_count = 2;
 static screen_display* screen_table;
 
-// API
+// API plumbing for eventual external plug-ins. For now this just makes it very easy to
+// add new screens to the program by adding a function elsewhere and referencing it once here.
+// This approach allows the main function to call screens directly. That might be overkill,
+// and we could simplify things by having all screens controlled internally here,
+// with a single indirection function exposed.
 void init_screen_table ()
 {
    screen_table = malloc(sizeof(screen_display)*screen_count);
    screen_table[0].name = "random";
-   screen_table[0].fun = random_matrix_screen;
+   screen_table[0].fun = random_screen;
    screen_table[1].name = "swirl";
    screen_table[1].fun = swirl_screen;
 }
@@ -28,110 +35,6 @@ screen_display* get_screen_table ()
 int get_screen_count ()
 {
    return screen_count;
-}
-
-// Completely random matrix of data
-long random_matrix_screen (int nrows, int ncols, int docolor)
-{
-   int r, c, attrb;
-
-   attron (A_BOLD);
-   for (r = 2; r < nrows - 2; ++r)
-   {
-      move (r, 0);
-      for (c = 0; c < ncols; ++c)
-      {
-         if (docolor)
-         {
-            attrb = COLOR_PAIR((rand () & 0x000D) + 1);
-            attron (attrb);
-         }
-         addch ((rand () & 0x3F) + 33);
-      }
-   }
-   attroff (A_BOLD);
-
-   // return frame bit count
-   if (docolor)
-   {
-      return 64*nrows*ncols;
-   }
-   else
-   {
-      return 8*nrows*ncols;
-   }
-}
-
-// Swirling dots display
-#define np 2048
-long swirl_screen (int nrows, int ncols, int docolor)
-{
-   int row, col, attrb;
-   static double rs[np], phis[np];
-   static int nrowslast, ncolslast;
-
-   if ((nrows == nrowslast) && (ncols == ncolslast))
-   {
-      // rotate
-      double speed = 0.0001, vorticity = 2.0;
-      for (register int k = 0; k < np; k++)
-      {
-         if (docolor)
-            phis[k] += speed/(vorticity*rs[k]/ncols+speed);
-         else
-            phis[k] += speed/(vorticity*rs[k]/ncols+speed);
-      }
-   }
-   else
-   {
-      // init
-      double uniformrv;
-      for (int k = 0; k < np; k++)
-      {
-         uniformrv = (double) rand() / (double) RAND_MAX;
-         rs[k] = uniformrv*ncols/2.0;
-         phis[k] = rand_max(359) / 3.1456 * 180.0;
-      }
-      nrowslast = nrows; ncolslast = ncols;
-   }
-
-   // clear
-   for (row = 2; row < nrows - 2; row++)
-   {
-      move (row, 0);
-      clrtoeol();
-   }
-
-   // write field
-   attron (A_BOLD);
-   int cycletime;
-   cycletime = (int) floor((double) np / 8.0);
-   for (register int k = 0; k < np; k++)
-   {
-      col = (int) round(rs[k]*cos(phis[k]) + ncols/2.0);
-      row = (int) round(rs[k]*sin(phis[k])/2.0 + nrows/2.0);
-      move (limiter(row, 2, nrows - 3), limiter(col, 1, ncols));
-      if (docolor && !(k % cycletime))
-      {
-         attrb = COLOR_PAIR((int) ceil((double) 8*k / (double) np) + 1);
-         attron(attrb);
-      }
-      if (qlimit (row, 1, nrows - 2))
-         addch ('.');  // in view
-      else
-         addch (' ');  // not in view
-   }
-   attroff (A_BOLD);
-
-   // return frame bit count
-   if (docolor)
-   {
-      return 32*np;
-   }
-   else
-   {
-      return 16*np;
-   }
 }
 
 // Track and display bitrate
@@ -176,7 +79,6 @@ void display_mbps (long bits, int nrows, int ncols, int warn, int reset)
       attroff(COLOR_PAIR(6));
    }
 }
-
 
 void static_display (int nrows, int ncols, int docolor, int verbose)
 {
--- timecurses.h
+++ timecurses.h
@@ -12,12 +12,6 @@ screen_display* get_screen_table ();
 
 int get_screen_count ();
 
-long sphere_screen (int nrows, int ncols, int docolor);
-
 void display_mbps (long bits, int nrows, int ncols, int warn, int reset);
 
 void static_display (int nrows, int ncols, int docolor, int verbose);
-
-long random_matrix_screen (int nrows, int ncols, int docolor);
-
-long swirl_screen (int nrows, int ncols, int docolor);
