9d14886f5d4a8a2aa791d640e4cef8776ea2ac39
Jonathan Birge <birge@mit.edu>
2022-02-18 03:51:37 -0500

    fix sphere algorithm

--- sphere_screen.c
+++ sphere_screen.c
@@ -12,7 +12,7 @@ long sphere_screen (int nrows, int ncols, int docolor)
    static int nrowslast, ncolslast;
    const double speed = 0.005;
    const int nlon = 15;  // number of lon lines
-   const double minlat = 20;  // deg of min lat
+   const double minlat = 10;  // deg of min lat
 
    if ((nrows == nrowslast) && (ncols == ncolslast))
    {
@@ -44,8 +44,9 @@ long sphere_screen (int nrows, int ncols, int docolor)
       for (row = 2; row < nrows - 2; row++)
       {
          double lat = minlat + (180.0 - minlat*2.0)*(row - 2)/(nrows - 5);
+         double latfrac = (lat - 90)/90;
          double phi0 = lon + phi;
-         double r = rho*sin(lat*DEGRAD);
+         double r = rho*sqrt(1 - latfrac*latfrac);
          int x = round(cos(phi0*DEGRAD)*r);
          int y = round(sin(phi0*DEGRAD)*r);
          if (y > 0)
