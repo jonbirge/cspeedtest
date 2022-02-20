5b77b7c646b9cf2cd77f2a37809598893e8eaf61
Jonathan Birge <birge@mit.edu>
2022-02-19 19:21:20 -0500

    clean up

--- test/helptest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Will be run from parent directory
-echo "Testing help..."
-src/cspeedtest --help > test/help.out
-grep Usage test/help.out
--- test/verstest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Will be run from parent directory
-echo "Testing version..."
-src/cspeedtest --version > test/vers.out
-grep cspeedtest test/vers.out
