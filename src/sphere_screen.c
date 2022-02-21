57c2d6d0f13d24b5de058cebd52a11948f3db556
Jonathan Birge <birge@mit.edu>
2022-02-21 01:34:46 -0500

    fix sphere draw order

--- src/sphere_screen.c
+++ src/sphere_screen.c
@@ -35,10 +35,12 @@ long sphere_screen (int nrows, int ncols, int docolor)
       clrtoeol();
    }
 
-   // write field
-   attron(COLOR_PAIR(8));
+   // *** write field ***
+   attron(COLOR_PAIR(8));  // for B/W mode
    int nchars = 0;
    char ch;
+
+   // background
    for (int lon = 0; lon < 360; lon += (int)floor(360/nlon))
    {
       double phi0 = (lon + phi);
@@ -47,26 +49,18 @@ long sphere_screen (int nrows, int ncols, int docolor)
       {
          if (phi0int < 180)
          {
-            attron(COLOR_PAIR((int)round((lon / (360.0/nlon))) % 8 + 9));
-            ch = ' ';
+            continue;
          }
          else
          {
-            attron(COLOR_PAIR((int)round((lon / (360.0/nlon))) % 8));
-            ch = '.';
+            attron(COLOR_PAIR((int)round((lon / (360.0/nlon))) % 8 + 1));
+            ch = ':';
          }
          nchars++;
       }
       else
       {
-         if (phi0int > 180)
-         {
-            continue;
-         }
-         else
-         {
-            ch = '*';
-         }
+         continue;  // low bandwidth mode doesn't show background
       }
       for (row = 2; row < nrows - 2; row++)
       {
@@ -80,20 +74,42 @@ long sphere_screen (int nrows, int ncols, int docolor)
       }
    }
 
-   // double rowsteps = 8;
-   // for (row = 2; row < nrows; row += floor(nrows/rowsteps))
-   // {
-   //    double lat = minlat + (180.0 - minlat*2.0)*(row - 2)/(nrows - 5);
-   //    double latfrac = (lat - 90)/90;
-   //    double r = rho*sqrt(1 - latfrac*latfrac);
-   //    move (row, round(ncols/2 - r));
-   //    for (int k = 0; k < 2*r; k++)
-   //    {
-   //       addch ('.');
-   //    }
-   // }
-   
-   attroff (A_BOLD);
+   // foreground
+   for (int lon = 0; lon < 360; lon += (int)floor(360/nlon))
+   {
+      double phi0 = (lon + phi);
+      double phi0int = (int)ceil(lon + phi) % 360;
+      if (docolor)
+      {
+         if (phi0int < 180)
+         {
+            attron(COLOR_PAIR((int)round((lon / (360.0/nlon))) % 8 + 9));
+            ch = ' ';
+         }
+         else
+         {
+            continue;
+         }
+         nchars++;
+      }
+      else
+      {
+         if (phi0int < 180)
+            ch = '*';
+         else
+            continue;
+      }
+      for (row = 2; row < nrows - 2; row++)
+      {
+         double lat = minlat + (180.0 - minlat*2.0)*(row - 2)/(nrows - 5);
+         double latfrac = (lat - 90)/90;
+         double r = rho*sqrt(1 - latfrac*latfrac);
+         int x = round(cos(phi0*DEGRAD)*r);
+         move (row, x + round(ncols/2));
+         addch (ch);
+         nchars++;
+      }
+   } 
 
    // return frame bit count
    return 8*nchars;
